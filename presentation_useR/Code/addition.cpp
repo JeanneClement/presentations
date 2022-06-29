098deb70a0e9cbf22e1c98d54d017f8fa043f6f2
JeanneClement <jeanne.clement16@laposte.net>
2022-06-29 20:29:58 +0200

    add presentations

--- presentation_useR/Code/addition.cpp
+++
@@ -1,6 +0,0 @@
-#include <Rcpp.h>
-using namespace Rcpp;
-// [[Rcpp::export]]
-int addition(int a, int b) {
-  return  a+b;
-}
--- presentation_useR/Code/draws.cpp
+++
@@ -1,4 +0,0 @@
-double x_prop = x_now + gsl_ran_gaussian_ziggurat(r, sigmap_beta(p));
-double u = gsl_rng_uniform(r);
-double x_prop = x_now + R::rnorm(0, sigmap_beta(p));
-double u = R::runif(0,1);
